c858f5fb20380a5f11f07331422d7f0b63aa4c57
HakanGnes <100923570+HakanGnes@users.noreply.github.com>
2022-12-14 17:04:51 +0300

    Update Kural Tabanlı Sınıflandırma ile Potansiyel Musteri Getirisi Hesaplama.py

--- Kural Tabanlı Sınıflandırma ile Potansiyel Musteri Getirisi Hesaplama.py
+++ Kural Tabanlı Sınıflandırma ile Potansiyel Musteri Getirisi Hesaplama.py
@@ -30,7 +30,6 @@
 
 # Soru 1: persona.csv dosyasını okutunuz ve veri seti ile ilgili genel bilgileri gösteriniz
 
-import numpy as np
 import pandas as pd
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 500)
